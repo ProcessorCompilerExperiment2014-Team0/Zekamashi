d8a20695d418be1c9e2f9109f26a9f3190e6dc88
zeptometer <yuito.murase@gmail.com>
2014-12-22 11:30:06 +0900

    core: fix component definition of zkms_mmu

--- core/components/mmu.vhd
+++ core/components/mmu.vhd
@@ -22,6 +22,22 @@ package zkms_mmu_p is
 
   component zkms_mmu is
     port (
+      zd     : inout std_logic_vector(31 downto 0);
+      zdp    : inout std_logic_vector(3 downto 0);
+      za     : out   std_logic_vector(19 downto 0);
+      xe1    : out   std_logic;
+      e2a    : out   std_logic;
+      xe3    : out   std_logic;
+      xzbe   : out   std_logic_vector(3 downto 0);
+      xga    : out   std_logic;
+      xwa    : out   std_logic;
+      xzcke  : out   std_logic;
+      zclkma : out   std_logic_vector(1 downto 0);
+      adva   : out   std_logic;
+      xft    : out   std_logic;
+      xlbo   : out   std_logic;
+      zza    : out   std_logic;
+
       din  : in  zkms_mmu_in_t;
       dout : out zkms_mmu_out_t);
   end component zkms_mmu;
