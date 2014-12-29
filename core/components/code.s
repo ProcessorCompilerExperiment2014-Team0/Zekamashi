a5f57764067ced538cb3b915a97be2e1abac230e
zeptometer <yuito.murase@gmail.com>
2014-12-29 22:44:59 +0900

    core: update components/code.s

--- core/components/code.s
+++ core/components/code.s
@@ -1,5 +1,9 @@
-        lda $2, 100($31)
+	lda $2, 100($31)
 loop:
-        addl $2, $0, $0
-        lda $2, -1($2)
-        bne $2, loop
+    addl $2, $0, $0
+    lda $2, -1($2)
+    bne $2, loop
+    ldah $3, 16($31)
+    stl $0, 3($3)
+stop:
+    br $31, stop
