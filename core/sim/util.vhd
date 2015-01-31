6e36f10f5c29e05c3d7ca3847287607cf7230396
zeptometer <yuito.murase@gmail.com>
2015-02-01 00:48:10 +0900

    core: add sim/util.vhd

---
+++ core/sim/util.vhd
@@ -0,0 +1,37 @@
+-------------------------------------------------------------------------------
+-- Declaration
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package util_p is
+
+  function is_metavalue (v : unsigned) return boolean;
+
+end package util_p;
+
+-------------------------------------------------------------------------------
+-- Definition
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package body util_p is
+
+  function is_metavalue (
+    v : unsigned)
+    return boolean is
+  begin
+    for i in v'range loop
+      if v(i) /= '0' and v(i) /= '1' then
+        return true;
+      end if;
+      return false;
+    end loop;
+  end function is_metavalue;
+
+end package body util_p;
